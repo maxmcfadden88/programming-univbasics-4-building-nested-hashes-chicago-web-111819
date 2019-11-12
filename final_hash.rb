c49aa98e6a1ef99b5ec3669dc887610a86db43da
Max M <maxmcfadden88@gmail.com>
2019-11-12 05:48:31 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -1,4 +1,4 @@
-# The final hash will look like this. Earlier methods will expect less as
+ # The final hash will look like this. Earlier methods will expect less as
 # you're suppose to "build up" to this.
 
 monopoly =  {
